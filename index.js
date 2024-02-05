5c38c67dbc0751ecfab198e8c37def8cc3f50b30
pankaj3649 <pankajrawat3649@gmail.com>
2024-02-05 18:24:42 +0530

    get api to get geners and popular movies

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/NetflixRoutes");
+const mongoose = require("mongoose");
+const app = express();
+ 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+
+
+app.use("/api", userRoutes);
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
---
+++ utils/constants.js
@@ -0,0 +1,5 @@
+module.exports = {
+    API_KEY : "a915ee2b1fced9ee9018f5a35886051c",
+    TMDB_BASE_URL :"https://api.themoviedb.org/3"
+}
+
