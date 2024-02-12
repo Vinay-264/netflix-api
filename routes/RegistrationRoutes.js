1bce9164fcffbd44f39d6f437c6e8fe9ebf1817f
Pankaj Rawat <36709366+pankaj3649@users.noreply.github.com>
2024-02-12 18:56:30 +0530

    Delete routes/RegistrationRoutes.js

--- routes/RegistrationRoutes.js
+++
@@ -1,8 +0,0 @@
-const {  registration } = require("../controllers/RegistrationController");
-
-
-const router = require("express").Router();
-
-router.post("/registration", registration);
-
-module.exports = router;
