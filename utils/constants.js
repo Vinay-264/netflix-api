7b58260a5f6ea4fcc8c3fe9234b16b892d5fba0e
pankaj3649 <pankajrawat3649@gmail.com>
2024-02-15 17:36:21 +0530

    rateLimit added

--- utils/constants.js
+++ utils/constants.js
@@ -3,6 +3,8 @@ module.exports = {
     TMDB_BASE_URL :"https://api.themoviedb.org/3",
     DB_USER_NAME:'pankajra',
     DB_PASSWORD:'Changenew14092034',
-    DB_NAME:'netflix'
+    DB_NAME:'netflix',
+    MAX_API_PER_TIME_FRAME: 100,
+    TIME_FRAME_PER_IP: 15 * 60 * 1000
 }
 
