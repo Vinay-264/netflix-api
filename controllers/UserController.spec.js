80077c47bb804228b66c77f7abc2dc9bd887c50f
Mohammad Mannan <mohammad.mannan@quovantis.com>
2024-02-19 16:04:27 +0530

    user controller test cases

--- controllers/UserController.spec.js
+++ controllers/UserController.spec.js
@@ -1,4 +1,7 @@
-const { getLikedMovies } = require("./UserController");
+const { getLikedMovies, addToLikedMovies, 
+    removeFromLikedMovies, getUserPreferences, 
+    saveUserPreferences, modifyUserPreferences,
+    getRecommendedContent, getContentNotification } = require("./UserController");
 
 const UserService = require("../services/UserService");
 
@@ -9,8 +12,8 @@ jest.mock("../services/UserService");
 describe("getLikedMovies", () => {
   it("should Invoke service layer", async () => {
 
+    // Mocking request and response objects
     const req = { params: { email: "test@abc.com" } };
-    //UserService.fetchLikedMovies.mockResolvedValue(req);
    
     const res = {
       status: jest.fn().mockReturnThis(),
@@ -26,7 +29,155 @@ describe("getLikedMovies", () => {
 
   });
 
-
-
-
 });
+
+  describe("addToLikedMovies", () => {
+    it("should Invoke service layer", async () => {
+  
+    // Mocking request and response objects
+
+      const req = { body: {"email": "xyz8998@gmail.com","data": {"id": 2,"genres":["comedy"],
+      "image":"./background.jpg","name":"pankaj"}} };
+     
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+        send: jest.fn(),
+      };
+  
+      // Calling the addToLikedMovies function
+      await addToLikedMovies(req, res);
+  
+      // Assertions
+      expect(UserService.addToLikedMovies).toHaveBeenCalled();
+  
+    });
+
+  });
+
+  describe("removeFromLikedMovies", () => {
+    it("should Invoke service layer", async () => {
+  
+        // Mocking request and response objects
+      const req = { body: { email: "test@abc.com", movieId:123 } };
+     
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+        send: jest.fn(),
+      };
+  
+      // Calling the removeFromLikedMovies function
+      await removeFromLikedMovies(req, res);
+  
+      // Assertions
+      expect(UserService.removedLikedMovies).toHaveBeenCalled();
+  
+    });
+  });
+
+
+  describe("getUserPreferences", () => {
+    it("should Invoke service layer", async () => {
+  
+        // Mocking request and response objects
+      const req = { params: { email: "test@abc.com"} };
+     
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+        send: jest.fn(),
+      };
+  
+      // Calling the getUserPreferences function
+      await getUserPreferences(req, res);
+  
+      // Assertions
+      expect(UserService.getUserPreferences).toHaveBeenCalled();
+  
+    });
+  });
+
+  describe("saveUserPreferences", () => {
+    it("should Invoke service layer", async () => {
+  
+        // Mocking request and response objects
+      const req = { body: { email: "test@abc.com", preferredGenres:123} };
+     
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+        send: jest.fn(),
+      };
+  
+      // Calling the saveUserPreferences function
+      await saveUserPreferences(req, res);
+  
+      // Assertions
+      expect(UserService.saveUserPreferences).toHaveBeenCalled();
+  
+    });
+  });
+
+  describe("modifyUserPreferences", () => {
+    it("should Invoke service layer", async () => {
+  
+        // Mocking request and response objects
+      const req = { param: { email: "test@abc.com"}, body:{preferredGenres:[123]} };
+     
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+        send: jest.fn(),
+      };
+  
+      // Calling the modifyUserPreferences function
+      await modifyUserPreferences(req, res);
+  
+      // Assertions
+      expect(UserService.modifyUserPreferences).toHaveBeenCalled();
+  
+    });
+  });
+
+  describe("getRecommendedContent", () => {
+    it("should Invoke service layer", async () => {
+  
+        // Mocking request and response objects
+      const req = { param: { email: "test@abc.com", category:123}};
+     
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+        send: jest.fn(),
+      };
+  
+      // Calling the getRecommendedContent function
+      await getRecommendedContent(req, res);
+  
+      // Assertions
+      expect(UserService.getRecommendedContent).toHaveBeenCalled();
+  
+    });
+  });
+
+  describe("getContentNotification", () => {
+    it("should Invoke service layer", async () => {
+  
+        // Mocking request and response objects
+      const req = { param: { category:123}};
+     
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+        send: jest.fn(),
+      };
+  
+      // Calling the getContentNotification function
+      await getContentNotification(req, res);
+  
+      // Assertions
+      expect(UserService.getContentNotification).toHaveBeenCalled();
+  
+    });
+  });
